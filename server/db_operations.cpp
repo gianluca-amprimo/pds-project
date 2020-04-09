27d0c01a600bd580ba5cc9829851c2f8dbf909a5
FoxAndre96 <s257387@studenti.polito.it>
2020-04-09 12:07:18 +0200

    Added function to delete an existing user and function to delete an existing file. Updated the function that reads the names of the existing files in order to update a data structure that contains the information on those files.

--- server/db_operations.cpp
+++ server/db_operations.cpp
@@ -1,12 +1,15 @@
 #include "db_operations.h"
 
-static int count = 0;
 static std::string db_path = "../pds_db";
+
+static int db_counter = 0;
+
+
 /*
  * Callback function for checkCredentials
  */
-static int check(void *NotUsed, int argc, char** argv, char** azColName){
-    count++;
+static int check(void *NotUsed, int argc, char **argv, char **azColName) {
+    db_counter++;
     return 0;
 }
 
@@ -14,11 +17,11 @@ static int check(void *NotUsed, int argc, char** argv, char** azColName){
 /*
  * Function to check if the user exists and its credentials are correct
  */
-int checkCredentials(std::string usr, std::string password){
+int checkCredentials(std::string usr, std::string password) {
     int rc;
     sqlite3 *db;
     std::string sql;
-    char* errMsg = nullptr;
+    char *errMsg = nullptr;
 
     // open database connection
     rc = sqlite3_open(db_path.c_str(), &db);
@@ -33,16 +36,17 @@ int checkCredentials(std::string usr, std::string password){
     sql = "SELECT * FROM USERS WHERE USERNAME = '" + usr + "' AND PASSWORD = '" + password + "'";
 
     // execute sql statement
-    count = 0;
+    db_counter = 0;
     rc = sqlite3_exec(db, sql.c_str(), check, nullptr, &errMsg);
-    if(rc != SQLITE_OK){
+    if (rc != SQLITE_OK) {
         std::cerr << "SQL error: " << errMsg << std::endl;
         sqlite3_free(errMsg);
+        sqlite3_close(db);
         return -1;
     }
     sqlite3_close(db);
 
-    if (count == 1) // username and password were correct
+    if (db_counter == 1) // username and password were correct
         return 1;
     return 0;
 }
@@ -51,18 +55,21 @@ int checkCredentials(std::string usr, std::string password){
 /*
  * Callback function for readFiles
  */
-static int files(void *NotUsed, int argc, char** argv, char** azColName){
-    int i = 0;
-    // TODO: mettere i file in una struttura
-    //  da mandare al client che possa visualizzare
-    //  a video
-    // stampo solo i nomi sul server per ora
-    // per vedere se funziona
-    std::cout << azColName[0] << " = " << argv[i];
-    for( i = 0; i < argc ; i++ ){
-        std::cout << ", " << azColName[i] << " = " << argv[i];
+static int files(void *NotUsed, int argc, char **argv, char **azColName) {
+    // check if argc is correct
+    if (argc != 3) {
+        std::cerr << "Error in the number of columns" << std::endl;
+        return -1;
     }
-    std::cout << std::endl;
+
+    // argc is 3 because the number of columns is 3
+    // ID, NAME, PATH
+
+    // create a tuple with name and path of the file
+    std::tuple<std::string, std::string> file = std::make_tuple(argv[1], argv[2]);
+
+    // add the tuple to the vector of files
+    file_list.insert(file);
     return 0;
 }
 
@@ -71,11 +78,11 @@ static int files(void *NotUsed, int argc, char** argv, char** azColName){
  * This function uses the files function as callback
  * function to modify the list of files structure
  */
-int readFiles(){
+int readFiles() {
     int rc;
     sqlite3 *db;
     std::string sql;
-    char* errMsg = nullptr;
+    char *errMsg = nullptr;
 
     // open database connection
     rc = sqlite3_open(db_path.c_str(), &db);
@@ -87,18 +94,19 @@ int readFiles(){
     }
 
     // prepare sql operation
-    sql = "SELECT * from FILES;";
+    sql = "SELECT * FROM FILES;";
 
     // execute sql statement
     rc = sqlite3_exec(db, sql.c_str(), files, nullptr, &errMsg);
-    if(rc != SQLITE_OK){
+    if (rc != SQLITE_OK) {
         std::cerr << "SQL error: " << errMsg << std::endl;
         sqlite3_free(errMsg);
+        sqlite3_close(db);
         return -1;
     }
     sqlite3_close(db);
 
-    if (count == 1) // username and password were correct
+    if (db_counter == 1) // username and password were correct
         return 1;
     return 0;
 }
@@ -107,11 +115,11 @@ int readFiles(){
 /*
  * function to add a new user in the database
  */
-int addUser(std::string user, std::string password, std::string name, std::string surname){
+int addUser(std::string user, std::string password, std::string name, std::string surname) {
     int rc;
     sqlite3 *db;
     std::string sql;
-    char* errMsg = nullptr;
+    char *errMsg = nullptr;
 
     // open database connection
     rc = sqlite3_open(db_path.c_str(), &db);
@@ -123,17 +131,21 @@ int addUser(std::string user, std::string password, std::string name, std::strin
     }
 
     //check the chosen username does not exist yet
-    sql="SELECT * FROM USERS WHERE username='"+user+"'";
-    count = 0;
+    sql = "SELECT * FROM USERS WHERE username='" + user + "'";
+    db_counter = 0;
     rc = sqlite3_exec(db, sql.c_str(), check, nullptr, &errMsg);
-    if(rc != SQLITE_OK){
+    if (rc != SQLITE_OK) {
         std::cerr << "SQL error: " << errMsg << std::endl;
         sqlite3_free(errMsg);
+        sqlite3_close(db);
         return -1;
     }
 
-    if (count == 1) // username already exists
+    if (db_counter == 1){
+        // username already exists
+        sqlite3_close(db);
         return 0;
+    }
 
     // prepare sql operation
     sql = "INSERT INTO USERS VALUES ('" + user + "', '"
@@ -143,8 +155,8 @@ int addUser(std::string user, std::string password, std::string name, std::strin
 
     // execute sql statement
     rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-    if(rc != SQLITE_OK){
-        std::cout << "SQL error: " << errMsg << std::endl;
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
         sqlite3_free(errMsg);
         sqlite3_close(db);
         return -1;
@@ -157,11 +169,11 @@ int addUser(std::string user, std::string password, std::string name, std::strin
 /*
  * function to add one file to the db
  */
-int addFile(std::string name, std::string path){
+int addFile(std::string name, std::string path) {
     int rc;
     sqlite3 *db;
     std::string sql;
-    char* errMsg = nullptr;
+    char *errMsg = nullptr;
 
     // open database connection
     rc = sqlite3_open(db_path.c_str(), &db);
@@ -172,21 +184,137 @@ int addFile(std::string name, std::string path){
         return -1;
     }
 
-    // prepare sql operation
-    sql = "INSERT INTO FILES ('NAME', 'PATH') VALUES ('" + name + "', '" + path + "')";
-
+    // prepare sql operation to see if the file does not already exist
+    sql = "SELECT * FROM FILES WHERE NAME = '" + name + "' AND PATH = '" + path + "'";
+    db_counter = 0;
     // execute sql statement
-    rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
-    if(rc != SQLITE_OK){
+    rc = sqlite3_exec(db, sql.c_str(), check, nullptr, &errMsg);
+    if (rc != SQLITE_OK) {
         std::cout << "SQL error: " << errMsg << std::endl;
         sqlite3_free(errMsg);
         sqlite3_close(db);
         return -1;
     }
+
+    if (db_counter == 1){
+        // file already exists
+        sqlite3_close(db);
+        return 0;
+    } else {
+        // prepare sql operation
+        sql = "INSERT INTO FILES ('NAME', 'PATH') VALUES ('" + name + "', '" + path + "')";
+
+        // execute sql statement
+        rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
+        if (rc != SQLITE_OK) {
+            std::cout << "SQL error: " << errMsg << std::endl;
+            sqlite3_free(errMsg);
+            sqlite3_close(db);
+            return -1;
+        }
+    }
     sqlite3_close(db);
     return 1;
 }
 
-int deleteUser(std::string username, std::string password){
-    //TODO: implement function to delete a user
+int deleteUser(std::string username, std::string password) {
+    int rc;
+    sqlite3 *db;
+    std::string sql;
+    char *errMsg = nullptr;
+
+    // open database connection
+    rc = sqlite3_open(db_path.c_str(), &db);
+
+    // check the connection has been established
+    if (rc != SQLITE_OK) {
+        std::cerr << "Can't open the database: " << sqlite3_errmsg(db) << std::endl;
+        return -1;
+    }
+
+    // prepare sql operation to check if user exists
+    sql = "SELECT * FROM USERS WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "';";
+
+    // execute sql statement
+    rc = sqlite3_exec(db, sql.c_str(), check, nullptr, &errMsg);
+    if (rc != SQLITE_OK) {
+        std::cout << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return -1;
+    }
+
+    if (db_counter == 1) {
+        // user exists
+        // prepare sql operation to delete the user
+        sql = "DELETE FROM USERS WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "';";
+
+        // execute sql statement
+        rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
+        if (rc != SQLITE_OK) {
+            std::cout << "SQL error: " << errMsg << std::endl;
+            sqlite3_free(errMsg);
+            sqlite3_close(db);
+            return -1;
+        }
+    } else {
+        // user doesn't exists or credentials are wrong
+        return 0;
+    }
+
+    sqlite3_close(db);
+    return 1;
+}
+
+
+int deleteFile(std::string name, std::string path) {
+    int rc;
+    sqlite3 *db;
+    std::string sql;
+    char *errMsg = nullptr;
+
+    // open database connection
+    rc = sqlite3_open(db_path.c_str(), &db);
+
+    // check the connection has been established
+    if (rc != SQLITE_OK) {
+        std::cerr << "Can't open the database: " << sqlite3_errmsg(db) << std::endl;
+        return -1;
+    }
+
+    // prepare sql operation to check if user exists
+    sql = "SELECT * FROM FILES WHERE NAME = '" + name + "' AND PATH = '" + path + "';";
+
+    // execute sql statement
+    rc = sqlite3_exec(db, sql.c_str(), check, nullptr, &errMsg);
+    if (rc != SQLITE_OK) {
+        std::cout << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return -1;
+    }
+
+    if (db_counter == 1) {
+        // file exists
+        // prepare sql operation to delete the file
+        sql = "DELETE FROM FILES WHERE NAME = '" + name + "' AND PATH = '" + path + "';";
+
+        // execute sql statement
+        rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &errMsg);
+        if (rc != SQLITE_OK) {
+            std::cout << "SQL error: " << errMsg << std::endl;
+            sqlite3_free(errMsg);
+            sqlite3_close(db);
+            return -1;
+        }
+
+        std::tuple<std::string, std::string> file = std::make_tuple(name, path);
+        file_list.erase(file);
+    } else {
+        // file does not exist or name and path are wrong
+        return 0;
+    }
+
+    sqlite3_close(db);
+    return 1;
 }
