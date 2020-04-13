931cfa9b36eedd9cc73fc9bd7c31d6ac6d5ed47e
altrimentidetto <francesco.chimienti@hotmail.com>
2020-04-13 11:09:07 +0200

    deleting old folders

--- Editor/build/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/Editor_autogen.dir/AutogenOldSettings.txt"
-  "CMakeFiles/Editor_autogen.dir/ParseCache.txt"
-  "Editor_autogen"
-)
--- Editor/cmake-build-debug/CMakeFiles/Editor_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Editor_autogen"
-  "Editor_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Editor_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
